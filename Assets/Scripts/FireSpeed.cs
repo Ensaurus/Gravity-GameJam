82bc913a8a1fbf32d420937dce01644ae80dda2c
michaelahwales <74111731+michaelahwales@users.noreply.github.com>
2022-06-04 16:07:45 -0400

    particle effect with max speed script

--- Assets/Scripts/FireSpeed.cs
+++ Assets/Scripts/FireSpeed.cs
@@ -7,11 +7,14 @@ public class FireSpeed : MonoBehaviour
     //var FireEffect Transform;
     //public FireEffect Transform;
     //ParticleSystem fireSystem = FireEffect;
-    
+    //public GameObject bodyPart;
+    public Rigidbody rb;
+    public float maxSpeed = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
+        //rb = bodyPart.GetComponent<Rigidbody>();
         ParticleSystem ps = GetComponent<ParticleSystem>();
         //var emiss = fireSystem.emission;
         var emiss = ps.emission;
@@ -25,12 +28,22 @@ public class FireSpeed : MonoBehaviour
         ParticleSystem ps = GetComponent<ParticleSystem>();
         //var emiss = fireSystem.emission;
         var emiss = ps.emission;
-        
+
+        /*
         if (Input.GetKey ("f")){
             emiss.enabled = true;
         }
         else{
             emiss.enabled = false;
         }   
+        */
+        Debug.Log(rb.velocity.magnitude); 
+
+        if (rb.velocity.magnitude >= maxSpeed){
+            emiss.enabled = true;
+        }
+        else{
+            emiss.enabled = false;
+        }
     }
 }
