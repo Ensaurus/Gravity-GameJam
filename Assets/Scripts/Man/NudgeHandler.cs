a000a076afc368ac4696a5dbcedc50d00f267d7f
Ensaurus <18ehm1@queensu.ca>
2022-06-04 15:31:45 -0400

    Added nudges and fixed bug of him moving through z axis

--- Assets/Scripts/Man/NudgeHandler.cs
+++ Assets/Scripts/Man/NudgeHandler.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 public class NudgeHandler : MonoBehaviour
 {
     private RagdollHandler ragdollHandler;
+    //public Rigidbody[] ragdollParts;
     public Rigidbody target;
-    public float nudgeStrength;
+    public float nudgeStrength = 50;
 
     private enum direction
     {
         left,
-        right
+        right,
+        up,
+        down
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        // ragdollParts = gameObject.GetComponentsInChildren<Rigidbody>();
         ragdollHandler = GetComponent<RagdollHandler>();
     }
 
@@ -33,6 +37,14 @@ public class NudgeHandler : MonoBehaviour
             {
                 Nudge(direction.right);
             }
+            else if (Input.GetKeyDown(KeyCode.W))
+            {
+                Nudge(direction.up);
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                Nudge(direction.down);
+            }
         }
     }
 
@@ -41,10 +53,18 @@ public class NudgeHandler : MonoBehaviour
         switch (dir)
         {
             case direction.left:
-                target.AddForce(Vector3.left, ForceMode.Impulse);
+                target.AddForce(Vector3.left * nudgeStrength, ForceMode.Impulse);
                 break;
             case direction.right:
-                target.AddForce(Vector3.right, ForceMode.Impulse);
+                target.AddForce(Vector3.right * nudgeStrength, ForceMode.Impulse);
+                break;
+            case direction.up:
+                target.AddForce(Vector3.up * nudgeStrength, ForceMode.Impulse);
+                break;
+            case direction.down:
+                target.AddForce(Vector3.down * nudgeStrength, ForceMode.Impulse);
+                break;
+            default:
                 break;
         }
     }         
