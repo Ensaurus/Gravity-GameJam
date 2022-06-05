4c74c4379b19b6812bfdc23875e1c2f218674c37
Ensaurus <18ehm1@queensu.ca>
2022-06-05 18:12:47 -0400

    Intro Improvements Switched to Spread Out Tiles

--- Assets/SeagullBehavior.cs
+++ Assets/SeagullBehavior.cs
@@ -27,7 +27,6 @@ public class SeagullBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(state);
         if (state == 0)
         {
             transform.Translate(0, 0, -Time.deltaTime * speed, Space.World);
